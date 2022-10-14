730caad050098582680d3794380f9952df224d27
sumanta ghosh <sumantaghosh5333@gmail.com>
2022-10-14 10:31:19 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/layout/Header";
+import Main from "./components/layout/Main";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
